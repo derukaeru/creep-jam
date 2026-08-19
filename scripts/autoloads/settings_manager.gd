76cad60bdb7534c780c61afd91f2918b240982fb
dalifyyy <d4lified@gmail.com>
2026-08-19 13:44:59 +0800

    feat: added settings functionality

--- scripts/autoloads/settings_manager.gd
+++ scripts/autoloads/settings_manager.gd
@@ -5,11 +5,30 @@ var music_strength: float = 0.5
 
 var fullscreen: bool = false
 
+func _ready() -> void:
+	apply_audio_settings()
+
 func set_sfx(value: float) -> void:
 	sound_effect_strength = value
+	apply_audio_settings()
 
 func set_music(value: float) -> void:
 	music_strength = value
+	apply_audio_settings()
 
 func set_fullscreen(value: bool) -> void:
 	fullscreen = value
+	apply_display_settings()
+
+func apply_audio_settings() -> void:
+	var music: int = AudioServer.get_bus_index("music")
+	var sfx: int = AudioServer.get_bus_index("sfx")
+	
+	AudioServer.set_bus_volume_db(music, linear_to_db(music))
+	AudioServer.set_bus_volume_db(sfx, linear_to_db(sfx))
+
+func apply_display_settings() -> void:
+	if fullscreen:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+	else:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
